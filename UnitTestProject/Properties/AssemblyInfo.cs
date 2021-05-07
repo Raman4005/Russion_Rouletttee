7a6321d721979d040007ba3a9aa5d7ffb76b19dc
Raman4005 <ramanbhallan668@gmail.com>
2021-05-08 09:52:55 +1200

    update

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("45daf8f5-514d-468b-b8b0-a4c8afdf6ac2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RussianRouletteAssignment;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Game Obj_game = new Game(); // Declare the object of the game class
+        [TestMethod]
+        public void RandomNumberPositive() // test to check bulletload variable value with 0 to 7
+        {
+            Obj_game.load();
+            Assert.IsTrue(Obj_game.bulletLoad < 7 && Obj_game.bulletLoad > 0);
+        }
+
+        // test to check bulletload variable value should not be greater than 7
+        [TestMethod]
+        public void RandomNumberNegative()
+        {
+            Obj_game.load();
+            Assert.IsFalse(Obj_game.bulletLoad > 7);
+        }
+    }
+}
